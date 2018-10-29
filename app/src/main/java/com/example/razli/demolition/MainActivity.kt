d61a65b272de43a32dac6d1f59b9fdea5e67cd38
Razli <razli1993@hotmail.com>
2018-10-29 22:00:13 +0800

    Remove junk comments

--- app/src/main/java/com/example/razli/demolition/MainActivity.kt
+++ app/src/main/java/com/example/razli/demolition/MainActivity.kt
@@ -8,9 +8,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // 29 Oct
-        // Can I push to master now?
-        // Test push 2
     }
 }
