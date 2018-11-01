7c2f45cefdee19cbe17adca115405286ab6d3177
Razli <razli1993@hotmail.com>
2018-11-01 20:55:30 +0800

    Basic Hello World RxJava example

--- app/src/main/java/com/example/razli/demolition/HomeActivity.java
+++ app/src/main/java/com/example/razli/demolition/HomeActivity.java
@@ -3,12 +3,57 @@ package com.example.razli.demolition;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 
 public class HomeActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textview);
+
+        Observable<String> observable = Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(ObservableEmitter<String> emitter) throws Exception {
+                emitter.onNext("I am going to master RxJava!");
+                emitter.onComplete();
+            }
+        });
+
+        Observer<String> subscriber = new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+                textView.setText("Received: " + s);
+            }
+
+            @Override
+            public void onError(Throwable t) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        };
+
+        observable.subscribe(subscriber);
     }
 }
